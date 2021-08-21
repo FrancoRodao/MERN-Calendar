2e1a594292a7e0dc7863c7e34b97096b1abcce56
FrancoRodao <francorodao802@gmail.com>
2021-08-20 23:24:32 -0300

    hotfix heroku deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,7 +32,7 @@ app.use(
 		origin: origins
 	})
 )
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(path.join(__dirname, '../dist')))
 app.use(express.json())
 app.use(cookieParser())
 
